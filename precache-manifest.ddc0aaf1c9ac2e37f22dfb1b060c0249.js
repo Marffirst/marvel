ed23f17cc8ed9c41acd29883e395110951781277
marff <Marffirstlove@gmail.com>
2021-10-29 09:25:36 +0300

    Add project build

---
+++ precache-manifest.ddc0aaf1c9ac2e37f22dfb1b060c0249.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e72d4fc615c63e664d7fd7cb0b6064",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12200331031e70e00433",
+    "url": "/js/app.4c0d05b8.js"
+  },
+  {
+    "revision": "a3873639a6223bac2cca",
+    "url": "/js/chunk-vendors.b53b4d9f.js"
+  },
+  {
+    "revision": "03b8ad800b22244878af354b0662e153",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
